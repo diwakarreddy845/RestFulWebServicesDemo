7b3fb275a5f4081d99f150971012b4d3222c97e9
Diwakar Reddy Gudisi <diwakarreddy45@gmail.com>
2017-11-14 00:08:35 +0800

    initial Commmit

---
+++ src/main/java/com/diwakar/spring/basics/restfuldemo/RestfuldemoApplication.java
@@ -0,0 +1,12 @@
+package com.diwakar.spring.basics.restfuldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfuldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfuldemoApplication.class, args);
+	}
+}
