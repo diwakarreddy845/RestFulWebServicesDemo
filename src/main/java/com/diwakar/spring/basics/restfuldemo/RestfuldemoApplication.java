0306d59edfef3bec6cf9eed573f3f32afa6a2312
c21757a <Nove@2017>
2017-11-19 11:51:32 +0800

    Hazelcast Changes

--- src/main/java/com/diwakar/spring/basics/restfuldemo/RestfuldemoApplication.java
+++ src/main/java/com/diwakar/spring/basics/restfuldemo/RestfuldemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestfuldemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfuldemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestfuldemoApplication.class, args);
+    }
 }
